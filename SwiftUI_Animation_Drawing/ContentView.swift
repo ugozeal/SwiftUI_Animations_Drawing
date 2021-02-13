c43b9fa643d956849e84e553c07ad766fdfdb49c
Ugozeal <okonkwodavidu@gmail.com>
2021-02-13 09:54:27 +0100

    Initial Commit

---
+++ SwiftUI_Animation_Drawing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUI_Animation_Drawing
+//
+//  Created by David U. Okonkwo on 2/13/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUI_Animation_Drawing/SwiftUI_Animation_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Animation_DrawingApp.swift
+//  SwiftUI_Animation_Drawing
+//
+//  Created by David U. Okonkwo on 2/13/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Animation_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
